107b2bbe8a98241a42ad8c7f07ca796c4977fcc4
Chethan <chethansuputhras1@gamil.com>
2021-03-22 13:47:00 +0530

    supporting Files

---
+++ SmartLightApp/Classes And Libraries/App Classes/SwitchesCell.h
@@ -0,0 +1,25 @@
+//
+//  SwitchesCell.h
+//  VithamasSocket
+//
+//  Created by Vithamas Technologies on 30/11/20.
+//  Copyright © 2020 Chethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SwitchesCell : UITableViewCell
+
+@property(nonatomic,strong)UILabel * lblONtime,* lblOFFtime,*lblAlarms,*lblON,*lbldays,*lblOFF;
+@property(nonatomic,strong)UILabel * lblBack,*lblLine,*lblWifiSetup,*lblLineParall;
+@property(nonatomic,strong)UIButton  * btnDay,*btnon ,*btnoff,*btnTime,*btnDelete,*btnONTimer,*btnOFFTimer,*btnRepeate;
+@property(nonatomic,strong)UIButton  *btn0,*btn1,*btn2,*btn3,*btn4,*btn5,*btn6,*btn7,*btnSave;
+@property(nonatomic,strong)NSMutableArray * dayArr;
+@property(nonatomic,strong)UIImageView * imgCheck;
+@property(nonatomic,strong)UIView *dayView;
+-(void)UpdateDaysStatus:(NSMutableDictionary *)dayDict;
+@end
+
+NS_ASSUME_NONNULL_END
